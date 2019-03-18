636c836fdefbce231ae83af28b18815469f18547
Lunnasi <sergey.shake@gmail.com>
2019-03-18 17:04:37 +0200

    added new staff

--- generate.py
+++ generate.py
@@ -2,15 +2,12 @@ import names
 import random
 import csv
 
-
-a = b = 0
-count = 1000000
+count = 100
 
 csv.register_dialect('myDialect',
 delimiter = ';',
 quoting=csv.QUOTE_NONE,
 skipinitialspace=True)
-
 
 def count_rows(n):
   while n != 0:
@@ -19,11 +16,10 @@ def count_rows(n):
 
 for x in range(count):
   row = count_rows(count)
-  if a % 100000 == 0:
-    b = a % 100000
-    print(str(b + " %" + " is done")
-  with open('./generated_values/person_'+ str(b) +'.txt', 'a+') as f:
+  if x % 10 == 0:
+    b = x / (count*0.01)
+    print(str(b) + " %" + " is done")
+  with open('person_'+ str(b) +'.csv', 'a+') as f:
     writer = csv.writer(f, dialect='myDialect', lineterminator ='\r')
     writer.writerow(next(row))
   f.close()
-  a += 1
