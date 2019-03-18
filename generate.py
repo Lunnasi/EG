d8f677dd604fc56328ecba52616d3b419e5924d1
Lunnasi <sergey.shake@gmail.com>
2019-03-18 16:42:38 +0200

    added new data

--- generate.py
+++ generate.py
@@ -2,24 +2,28 @@ import names
 import random
 import csv
 
-person = []
 
 a = b = 0
+count = 1000000
 
 csv.register_dialect('myDialect',
 delimiter = ';',
 quoting=csv.QUOTE_NONE,
 skipinitialspace=True)
 
-for x in range(1000000):
-  person.append([names.get_first_name(), names.get_last_name(), random.randint(10, 80)])
+
+def count_rows(n):
+  while n != 0:
+    yield (names.get_first_name() ,names.get_last_name(), random.randint(10, 80))
+    n -= 1
+
+for x in range(count):
+  row = count_rows(count)
+  if a % 100000 == 0:
+    b = a % 100000
+    print(str(b + " %" + " is done")
+  with open('./generated_values/person_'+ str(b) +'.txt', 'a+') as f:
+    writer = csv.writer(f, dialect='myDialect', lineterminator ='\r')
+    writer.writerow(next(row))
+  f.close()
   a += 1
-  if a == 10000:
-      b = b + 1
-      print(str(b)+" %")
-      a = 0 
-      if b % 10 == 0:
-        with open('./generated_values/person_'+ str(b) +'.csv', 'a+') as f:
-            writer = csv.writer(f, dialect='myDialect', lineterminator ='\r')
-            for row in person: writer.writerow(row)
-        f.close()
